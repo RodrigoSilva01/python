bc41dca19ed9575a0aa3fdfdc37c647234efa1ff
Rodrigo Silva <rodrigosaints202@gmail.com>
2026-09-23 15:34:38 -0300

    Atualizei o ex034

--- mundo01/ex034.py
+++ mundo01/ex034.py
@@ -5,4 +5,4 @@ dw_1250 = salary * 15 / 100
 if salary > 1250:
     print(f'Seu salário é de R${salary:.2f} e teve um aumento de 10%, passando para R${salary + up_1250:.2f}')
 else:
-    print(f'Seu salário é de R${salary:.2f} e teve um aumento de 15%, passando para R${salary + dw_1250:.2f"}')
+    print(f'Seu salário é de R${salary:.2f} e teve um aumento de 15%, passando para R${salary + dw_1250:.2f}')
